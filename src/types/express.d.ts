22e1c068349207af10fd257828d240f575098e42
deva <devasharjin@gmail.com>
2026-09-24 14:25:31 +0530

    updated

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,18 +1,21 @@
 import "express";
 import { JwtPayload } from "jsonwebtoken";
+import { UserRole } from "../models/auth/user.model";
 
 export interface UserPayload extends JwtPayload {
+  _id?: string;
   userId?: string;
   id?: string;
   email?: string;
   name?: string;
-  role?: string[] | string;
+  role?: UserRole[] | string[] | UserRole | string;
+  phone?: string;
 }
 
 declare global {
   namespace Express {
     interface Request {
-      user?: UserPayload | string;
+      user?: UserPayload;
     }
   }
 }
